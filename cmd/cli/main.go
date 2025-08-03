1d67fb00a8f2e011c58bf26a2cf2c5abd10bc3e3
Agustin Pagotto <agustinpagotto@gmail.com>
2025-08-03 15:01:36 -0300

    add cli functionality

---
+++ cmd/cli/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/url"
+)
+
+func main() {
+	var urlFromCli string
+	var depthCrawl int
+
+	flag.StringVar(&urlFromCli, "url", "http://google.com", "Url to be Crawled")
+	flag.StringVar(&urlFromCli, "u", "http://google.com", "Url to be Crawled")
+	flag.IntVar(&depthCrawl, "depth", 1, "Depth of the crawl")
+	flag.IntVar(&depthCrawl, "d", 1, "Depth of the crawl")
+	flag.Parse()
+	if depthCrawl < 1 {
+		log.Fatalf("The depth of crawl can't be less than 1")
+	}
+	if urlFromCli == "" {
+		log.Fatalf("Please provide a non empty url")
+	}
+	parsedUrl, err := url.ParseRequestURI(urlFromCli)
+	if err != nil {
+		log.Fatalf("url inserted is invalid: %s", err)
+	}
+	fmt.Println(
+		"url:", parsedUrl.String(),
+		"err:", err,
+		"depth:", depthCrawl,
+	)
+}
